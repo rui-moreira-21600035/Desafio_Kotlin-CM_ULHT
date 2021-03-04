d40c4b8cdefa2a5a53d31e88c9ccd30d6d231dec
Rui Moreira - 21600035 <a21600035@alunos.ulht.pt>
2021-03-04 20:56:09 +0000

    Major update

--- src/pt/ulusofona/cm/kotlin/challenge/interfaces/Ligavel.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/interfaces/Ligavel.kt
@@ -2,3 +2,13 @@ package pt.ulusofona.cm.kotlin.challenge.interfaces
 
 import pt.ulusofona.cm.kotlin.challenge.exceptions.*
 
+interface Ligavel {
+    @Throws(VeiculoLigadoException::class)
+    fun ligar()
+
+    @Throws(VeiculoDesligadoException::class)
+    fun desligar()
+
+    fun estaLigado(): Boolean
+
+}
